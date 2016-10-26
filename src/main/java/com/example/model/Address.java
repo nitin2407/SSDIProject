c8b9d3f093ae19b6729c1f1da00adb57be6b78ce
Nitin Purohit <Nitin Purohit>
2016-10-26 00:40:05 -0400

    controller for signUp page added

---
+++ src/main/java/com/example/model/Address.java
@@ -0,0 +1,53 @@
+package com.example.model;
+
+/**
+ * Created by nitin on 10/25/2016.
+ */
+public class Address {
+
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String zip_code;
+    private String state;
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZip_code() {
+        return zip_code;
+    }
+
+    public void setZip_code(String zip_code) {
+        this.zip_code = zip_code;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
