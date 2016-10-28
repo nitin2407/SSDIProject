751b37c6216eb957dd1fbbbdf884df9874b38407
Nitin Purohit <Nitin Purohit>
2016-10-28 03:47:55 -0400

    added profile page

--- src/main/java/com/example/controllers/Login.java
+++ src/main/java/com/example/controllers/Login.java
@@ -115,4 +115,62 @@ public class Login{
         //return access;
     }
 
+    @RequestMapping(path="/profile",method = RequestMethod.GET,produces= MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody User sendUserDetails(HttpSession userSession, HttpServletRequest request) {
+
+        User user = new User();
+        Address address=new Address();
+        user.setUsername((String) userSession.getAttribute("username"));
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/showyourskillz", "root", "nitin4192");
+            PreparedStatement pst = conn.prepareStatement("SELECT * from user where user_name= ?");
+            pst.setString(1,user.getUsername());
+            ResultSet rs = pst.executeQuery();
+            if (rs.next()) {
+                user.setPassword(rs.getString("password"));
+                user.setFname(rs.getString("fname"));
+                user.setLname(rs.getString("lname"));
+                user.setPhoneNumber(rs.getString("phone_no"));
+                address.setAddressLine1(rs.getString("addr1"));
+                address.setAddressLine2(rs.getString("addr2"));
+                address.setCity(rs.getString("city"));
+                address.setState(rs.getString("state"));
+                address.setZip_code(rs.getString("zip_code"));
+                user.setAddress(address);
+                rs.close();
+                return user;
+            } else {
+                rs.close();
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
+
+    @RequestMapping(path="/profile",method = RequestMethod.POST,consumes= MediaType.APPLICATION_JSON_VALUE)
+    public void editUser(@RequestBody User user,HttpSession userSession, HttpServletRequest request) {
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/showyourskillz", "root", "nitin4192");
+            PreparedStatement pst = conn.prepareStatement("Update user set fname=?,lname=?,password=?,addr1=?,addr2=?,city=?,state=?,zip_code=?,phone_no=? where user_name=?");
+            pst.setString(1, user.getFname());
+            pst.setString(2, user.getLname());
+            pst.setString(3, user.getPassword());
+            pst.setString(4, (user.getAddress()).getAddressLine1());
+            pst.setString(5, (user.getAddress()).getAddressLine2());
+            pst.setString(6, (user.getAddress()).getCity());
+            pst.setString(7, (user.getAddress()).getState());
+            pst.setString(8, (user.getAddress()).getZip_code());
+            pst.setString(9, user.getPhoneNumber());
+            pst.setString(10, (String) userSession.getAttribute("username"));
+            pst.executeUpdate();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
